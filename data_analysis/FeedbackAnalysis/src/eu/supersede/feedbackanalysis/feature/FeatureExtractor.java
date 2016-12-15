a603b61a1742ec7b39332591620efc68541ac476
Sergi Nadal <snadal@essi.upc.edu>
2016-12-15 10:31:12 +0100

    update interfaces

--- data_analysis/FeedbackAnalysis/src/eu/supersede/feedbackanalysis/feature/FeatureExtractor.java
+++ data_analysis/FeedbackAnalysis/src/eu/supersede/feedbackanalysis/feature/FeatureExtractor.java
@@ -10,11 +10,22 @@ import eu.supersede.feedbackanalysis.ds.UserFeedback;
 public interface FeatureExtractor {
 
 	/**
+	 * Generates the feature file in pathOut given a batch set of feedbacks in pathIn
+	 * This method is meant to be called in a batch manner (e.g., once a day)
 	 *
-	 * @param userFeedback
-	 * @return
+	 * @param pathIn file with the training set
+	 * @param pathOut file where to store the model
+	 * @return true if everything went fine
 	 */
-	public boolean extractFeatures
+	public boolean batch(String pathIn, String pathOut);
 
-	public FeatureExtractionResult extractFeatures(List<UserFeedback> userFeedback);
+	/**
+	 * Generates the feature file in pathOut given a batch set of feedbacks in pathIn
+	 * This method is meant to be called in a real-time manner (i.e., as soon as one feedback arrives)
+	 *
+	 * @param featurePath file with the batch of features
+	 * @param userFeedback a user feedback to analyze
+	 * @return extracted features for userFeedback
+	 */
+	public FeatureExtractionResult single(String featurePath, UserFeedback userFeedback);
 }
--- data_analysis/FeedbackAnalysis/src/eu/supersede/feedbackanalysis/sentiment/SentimentAnalyzer.java
+++ data_analysis/FeedbackAnalysis/src/eu/supersede/feedbackanalysis/sentiment/SentimentAnalyzer.java
@@ -9,7 +9,8 @@ public interface SentimentAnalyzer {
 
 	/**
 	 * Trains a Sentiment Analysis model reading the training set from pathIn and storing the model in pathOut
-	 *
+	 * This method is meant to be called in a batch manner (e.g., once a day)
+
 	 * @param pathIn file with the training set
 	 * @param pathOut file where to store the model
 	 * @return true if everything went fine
@@ -18,13 +19,15 @@ public interface SentimentAnalyzer {
 	public boolean train (String pathIn, String pathOut) throws Exception;
 
 	/**
-	 * Classifies the list of input user feedbacks
-	 *
-	 * @param userFeedback
-	 * @return
+	 * Classifies the list of input user feedbacks evaluating the model stored in modelPath
+	 * This method is meant to be called in a real-time manner (i.e., as soon as one feedback arrives)
+
+	 * @param modelPath the path where the model is stored
+	 * @param userFeedback a user feedback to analyze
+	 * @return sentiment analysis result for userFeedback
 	 * @throws Exception
 	 */
-	public List<SentimentAnalysisResult> classify(List<UserFeedback> userFeedback) throws Exception;
+	public SentimentAnalysisResult classify(String modelPath, UserFeedback userFeedback) throws Exception;
 
 
 	// TODO: to be removed
