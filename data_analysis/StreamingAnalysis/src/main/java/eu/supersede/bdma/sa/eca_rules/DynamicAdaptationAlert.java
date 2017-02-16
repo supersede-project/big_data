e5d05839aa225f7317266231e5f3d8d9272fd711
Sergi Nadal <snadal@essi.upc.edu>
2017-02-16 18:52:29 +0100

    spark changes

--- data_analysis/StreamingAnalysis/src/main/java/eu/supersede/bdma/sa/eca_rules/DynamicAdaptationAlert.java
+++ data_analysis/StreamingAnalysis/src/main/java/eu/supersede/bdma/sa/eca_rules/DynamicAdaptationAlert.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class DynamicAdaptationAlert {
 
     public static void sendAlert() throws Exception {
+        /*
         List<Condition> conditions = new ArrayList<>();
         List<ActionOnFeature> actionsF = new ArrayList<>();
         List<ActionOnAttribute> actionsA = new ArrayList<>();
@@ -27,5 +28,7 @@ public class DynamicAdaptationAlert {
 
         TopicPublisher publisher = new TopicPublisher(SubscriptionTopic.ANALISIS_DM_ADAPTATION_EVENT_TOPIC,true);
         publisher.publishTextMesssageInTopic(new Gson().toJson(alert));
+        */
+        System.out.println("Im sending a dynamic adaptation alert!");
     }
 }
