063c79b1fb2b541617dc82dc6e9aa9e22e05e96b
Emitza Guzman <emitza@gmail.com>
2017-03-14 19:20:16 +0100

    adding missing keywordsearchresult file

---
+++ data_analysis/FeedbackAnalysis/src/main/java/eu/supersede/feedbackanalysis/ds/KeywordSearchResult.java
@@ -0,0 +1,19 @@
+package eu.supersede.feedbackanalysis.ds;
+
+import java.util.List;
+
+/**
+ * Created by Emitza on 3/14/17.
+ */
+public class KeywordSearchResult {
+
+    private List<String> foundKeywords;
+
+    public List<String> getFoundKeywords() {
+        return foundKeywords;
+    }
+
+    public void setFoundKeywords(List<String> foundKeywords) {
+        this.foundKeywords = foundKeywords;
+    }
+}
