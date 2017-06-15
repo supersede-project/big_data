bd4bf278f43e2f8889a8143cc6f325268819f62b
Sergi Nadal <snadal@essi.upc.edu>
2017-06-15 16:50:47 +0200

    ready for production server

--- data_management/MetadataManagementSystem/MetadataFrontend/config.js
+++ data_management/MetadataManagementSystem/MetadataFrontend/config.js
@@ -6,7 +6,7 @@ var config = {};
 
 config.PORT = 3000;
 config.METADATA_FRONTEND_URL = "http://localhost:"+config.PORT+"/";
-config.METADATA_DATA_LAYER_URL = "http://localhost:8081/metadataStorage/";
+config.METADATA_DATA_LAYER_URL = "http://localhost:8082/metadataStorage/";
 config.ONTO_MATCH_MERGE_URL = "http://localhost:8082/ontoMatchMerge/";
 config.DEFAULT_NAMESPACE = "http://supersede/";
 
