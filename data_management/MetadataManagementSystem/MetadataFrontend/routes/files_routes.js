ce6ca5d61814895b59ceb60387b86790f74c7ec3
Sergi Nadal <snadal@essi.upc.edu>
2017-09-21 18:05:44 +0200

    WP1 apis

---
+++ data_management/MetadataManagementSystem/MetadataFrontend/routes/files_routes.js
@@ -0,0 +1,25 @@
+/**
+ * Created by snadal on 17/05/16.
+ */
+
+var request = require('request'),
+    config = require(__dirname+'/../config'),
+    fs = require('fs');
+
+
+exports.postFile = function (req, res, next) {
+    if (!(req.body.hasOwnProperty('path')) || req.body.path==null
+        || !(req.files.hasOwnProperty('file')) || req.files.file==null){
+        res.status(400).json({msg: "(Bad Request) data format: {path, file}"});
+    } else {
+        var tmp_path = req.files.file.path;
+        var target_path = config.FILES_PATH + req.files.file.originalname;
+        fs.rename(tmp_path, target_path, function(err) {
+            if (err) throw err;
+            fs.unlink(tmp_path, function() {
+                if (err) throw err;
+                res.send('File uploaded to: ' + target_path);
+            });
+        });
+    }
+};
