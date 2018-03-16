9267ddfb4f5979d892682363a97c58d4ff682248
Fitsum Kifetew <kifetew@fbk.eu>
2018-03-16 12:43:48 +0100

    implemented oversampling of minority classes during feedback classification model training using the SMOTE method

--- data_analysis/FeedbackAnalysis/src/main/java/eu/supersede/feedbackanalysis/classification/SpeechActBasedClassifier.java
+++ data_analysis/FeedbackAnalysis/src/main/java/eu/supersede/feedbackanalysis/classification/SpeechActBasedClassifier.java
@@ -6,17 +6,22 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Attribute;
+import weka.core.AttributeStats;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
 import eu.supersede.feedbackanalysis.ds.ClassificationResult;
 import eu.supersede.feedbackanalysis.ds.UserFeedback;
 import eu.supersede.feedbackanalysis.preprocessing.utils.AttributeExtractor;
+import eu.supersede.feedbackanalysis.preprocessing.utils.SMOTE;
+import eu.supersede.feedbackanalysis.preprocessing.utils.Utils;
 
 public class SpeechActBasedClassifier implements FeedbackClassifier {
 
@@ -80,6 +85,8 @@ public class SpeechActBasedClassifier implements FeedbackClassifier {
 			options = weka.core.Utils.splitOptions("-P 100 -I 100 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1");
 			rf.setOptions(options);
 			instances.setClassIndex(instances.numAttributes() - 1);
+			// apply SMOTE for oversampling of minority classes
+			instances = applyOversampling(instances);
 			rf.buildClassifier(instances);
 			
 			// serialize the model to file
@@ -95,6 +102,27 @@ public class SpeechActBasedClassifier implements FeedbackClassifier {
 		return result;
 	}
 
+	public Instances applyOversampling (Instances instances) throws Exception {
+		AttributeStats attributeStats = instances.attributeStats(instances.classIndex());
+		int[] counts = attributeStats.nominalCounts;
+		Arrays.sort(counts);
+		double max = Utils.max(counts);
+		// iterate over the labels, calculate the percentage, and resample
+		// heruistic: if attr1 < 0.8*max ==> resample by ((max-attr1)/attr1)*100
+		double threshold = 0.8; //80%
+		for (int i = 0; i < counts.length - 1; i++) {
+			double w = counts[i];
+			if (threshold * max > w) {
+				SMOTE smote = new SMOTE();
+				smote.setInputFormat(instances);
+				double percentage = (max - w)/w * 100;
+				smote.setPercentage(percentage);
+				instances = Filter.useFilter(instances, smote);
+			}
+		}
+		return instances;
+	}
+	
 	public static void main(String[] args) throws Exception {
 		//  for testing purposes only 
 		
