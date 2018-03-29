d96e685c8e3ae80e2e6df6cbdfb88e72e2bcab0d
Fitsum Kifetew <kifetew@fbk.eu>
2018-03-29 15:56:06 +0200

    added cluster evaluation method (Rand index)

--- data_analysis/FeedbackAnalysis/src/main/java/eu/supersede/feedbackanalysis/clustering/FeedbackClusterer.java
+++ data_analysis/FeedbackAnalysis/src/main/java/eu/supersede/feedbackanalysis/clustering/FeedbackClusterer.java
@@ -11,11 +11,13 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
+import java.util.Map.Entry;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.jena.ontology.OntClass;
@@ -154,12 +156,12 @@ public class FeedbackClusterer {
 		loadDataset(fvs.toString(), arff, file);
 		
 		// save fv
-		try {
-			String fvFile = "src/test/resources/trainingsets/SENERCON_fv.csv";
-			FileUtils.writeStringToFile(new File(fvFile), fvs.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		try {
+//			String fvFile = "src/test/resources/trainingsets/SENERCON_fv.csv";
+//			FileUtils.writeStringToFile(new File(fvFile), fvs.toString());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 		
 		// compute clusters
 		SimpleKMeans clusterer = computeClusters(numClusters);
@@ -179,6 +181,27 @@ public class FeedbackClusterer {
 				feedbackClusters.put(cluster, new ArrayList<FeedbackMessage>());
 			}
 			feedbackClusters.get(cluster).add(userFeedback);
+		}
+		return feedbackClusters;
+	}
+	
+	/**
+	 * Maps every user feedback to a cluster
+	 * @param allFeedback
+	 * @param clusterer
+	 * @return a Map with key user feedback and value the cluster id
+	 * @throws Exception
+	 */
+	public Map<FeedbackMessage, Integer> clusterUserFeedback3 (List<FeedbackMessage> allFeedback, SimpleKMeans clusterer) throws Exception{
+		Map<FeedbackMessage, Integer> feedbackClusters = new HashMap<FeedbackMessage, Integer>();
+		for (FeedbackMessage userFeedback : allFeedback) {
+			Set<OntClass> concepts = feedbackAnnotator.annotateFeedback2(userFeedback);
+			int[] fv = ontologyWrapper.conceptsToFeatureVector(concepts);
+			double weight = instances.attribute(0).weight();
+			Instance instance = new DenseInstance(weight, Arrays.stream(fv).asDoubleStream().toArray());
+			int cluster = clusterer.clusterInstance(instance);
+			
+			feedbackClusters.put(userFeedback, cluster);
 		}
 		return feedbackClusters;
 	}
@@ -224,20 +247,92 @@ public class FeedbackClusterer {
 		}
 		return nearestNeighbours;
 	}
+
+	/**
+	 * compute metrics for comparing the computed clusters with the ones provided by the expert (gold standard) 
+	 * @param feedbackClusters a map with clusters as key and feedbacks in the cluster as value
+	 */
+	public double computeClusteringAccuracy(Map<FeedbackMessage, Integer> feedbackClusters){
+		
+		/**
+		 * S11 = {pairs that are in the same cluster under C and C’}
+		 * S00 = {pairs that are in different clusters under C and C’}
+		 * S10 = {pairs that are in the same cluster under C but in different ones under C’}
+		 * S01 = {pairs that are in different clusters under C but in the same under C’}
+		 */
+//		int[][] counts = new int [2][2];
+		Set<String> s11 = new HashSet<String>();
+		Set<String> s01 = new HashSet<String>();
+		Set<String> s10 = new HashSet<String>();
+		Set<String> s00 = new HashSet<String>();
+		
+		// build a list of feedback objects
+		List<FeedbackMessage> feedbackMessages = new ArrayList<>();
+		feedbackMessages.addAll(feedbackClusters.keySet());
+		
+		for (int i = 0; i < feedbackMessages.size(); i++) {
+			FeedbackMessage f1 = feedbackMessages.get(i);
+			int c1 = feedbackClusters.get(f1);
+			for (int j = i + 1; j < feedbackMessages.size(); j++) {
+				FeedbackMessage f2 = feedbackMessages.get(j);
+				int c2 = feedbackClusters.get(f2);
+				
+				String pair = "{" + f1.getId() + "," + f2.getId() + "}";
+				if (c1 == c2 && f1.getClusterId().equalsIgnoreCase(f2.getClusterId())) { // S11
+					s11.add(pair);
+				} else if (c1 == c2 && !f1.getClusterId().equalsIgnoreCase(f2.getClusterId())) { // S10
+					s10.add(pair);
+				} else if (c1 != c2 && f1.getClusterId().equalsIgnoreCase(f2.getClusterId())) { // S01
+					s01.add(pair);
+				} else { // S00
+					s00.add(pair);
+				}
+			}
+		}
+		
+		double score = randIndex (s11, s01, s10, s00, feedbackMessages.size());
+		
+		return score;
+	}
 	
+	/**
+	 * @param s11
+	 * @param s01
+	 * @param s10
+	 * @param s00
+	 * @return
+	 */
+	private double randIndex(Set<String> s11, Set<String> s01, Set<String> s10, Set<String> s00, int numEntries) {
+//		double totalPairs = s11.size() + s00.size() + s10.size() + s01.size();
+		return 2* (s11.size() + s00.size()) / (double)(numEntries * (numEntries -1 ));
+	}
+
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		String dataset = "trainingsets/SDO_ontology.ttl.class_only.fv.csv";
-		boolean arff = false;
-		boolean file = true;
-		double percent = 0.7;
-		FeedbackClusterer fCluster = new FeedbackClusterer(dataset, arff, file, percent);
-		fCluster.computeClusters(5);
+	public static void main(String[] args)  throws Exception {
+		String csvPath = "trainingsets/SENERCON_userfeedback_clustering.csv"; //SENERCON_userfeedback_clustering_old.csv";
+		String ontologyFile = "SDO_ontology.ttl";
+		String wordnetDbPath = null;
+		String language = "en";
 		
-		Instances nearestNeighbors = fCluster.computeNearestNeighbors(5);
-
+		List<FeedbackMessage> feedbackMessages = FeedbackAnnotator.getFeedbackMessagesForClustering(csvPath);
+		
+		FeedbackClusterer feedbackClusterer = new FeedbackClusterer(ontologyFile, wordnetDbPath, language);
+		int numClusters = 40;
+		SimpleKMeans clusterer = feedbackClusterer.computeClusters2(feedbackMessages, numClusters);
+		Map<FeedbackMessage, Integer> feedbackClusters = feedbackClusterer.clusterUserFeedback3(feedbackMessages, clusterer);
+		double accuracy = feedbackClusterer.computeClusteringAccuracy(feedbackClusters);
+		System.out.println(accuracy);
+//		String dataset = "trainingsets/SDO_ontology.ttl.class_only.fv.csv";
+//		boolean arff = false;
+//		boolean file = true;
+//		double percent = 0.7;
+//		FeedbackClusterer fCluster = new FeedbackClusterer(dataset, arff, file, percent);
+//		fCluster.computeClusters(5);
+//		
+//		Instances nearestNeighbors = fCluster.computeNearestNeighbors(5);
+		
 	}
 
 }
