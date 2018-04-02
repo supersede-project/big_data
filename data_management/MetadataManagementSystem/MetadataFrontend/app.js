2918f8ae1e21e53ad8fef91efa7635edfabbb9c5
Sergi Nadal <snadal@essi.upc.edu>
2018-04-02 12:42:23 +0200

    add HTTP headers

--- data_management/MetadataManagementSystem/MetadataFrontend/app.js
+++ data_management/MetadataManagementSystem/MetadataFrontend/app.js
@@ -45,6 +45,22 @@ var classification_routes = require(__dirname+'/routes/classification_routes');
 /*****************************************************************************************/
 /*****************************************************************************************/
 // view engine setup
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(logger('dev'));
